a1867dd79cfe7968b9eedba7d0ec18517ecf9b33
YunChaeHyeon <cogus0923@nate.com>
2024-02-05 14:31:45 +0900

    chip-navigationBar (Fragment) 연동

--- MyDailyApp/app/src/main/java/com/example/mydailyapp/MainActivity.kt
+++ MyDailyApp/app/src/main/java/com/example/mydailyapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.mydailyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import com.example.mydailyapp.weather_Fragment
+import com.example.mydailyapp.accountBook_Fragment
+import com.example.mydailyapp.TodoList_Fragment
+
 
 class MainActivity : AppCompatActivity() {
+
+    val fragment = weather_Fragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        openMainFragment()
+        supportActionBar?.hide()
+
+        var menu_bottom = findViewById<ChipNavigationBar>(R.id.bottom_nav_bar)
+        menu_bottom.setItemSelected(R.id.nav_weather)
+
+        menu_bottom.setOnItemSelectedListener {
+            when(it){
+                R.id.nav_weather -> {
+                    openMainFragment()
+                }
+                R.id.nav_accountBook -> {
+                    val accountBookFragment = accountBook_Fragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frag_container_nav, accountBookFragment).commit()
+                }
+                R.id.nav_todoList -> {
+                    val todoListFragment = TodoList_Fragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frag_container_nav, todoListFragment).commit()
+                }
+            }
+        }
+    }
+
+    private fun openMainFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frag_container_nav, fragment)
+        transaction.commit()
     }
 }
